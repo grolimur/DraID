c706e65015ade77a01116a8357d0316e14ac764a
grolimur <grolimur@nous4.ch>
2018-04-05 21:39:17 +0200

    test several DOIs added (v0.1)

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+i<!DOCTYPE html>
 <html>
 <head>
 <link type="text/css" rel="stylesheet" href="styles.css" />
@@ -6,32 +6,39 @@
 <body>
   <h1>DOI registration agency IDentifier</h1>
   <form action="index.php" method="get">
-  Copy a DOI here: <input name="d" type="text">
+  <textarea name="d" placeholder="Copy DOI here"></textarea>
   <input type="submit" value="Submit">
   </form>
   <br/>
   <?php
 
   $d = $_GET[d];
-  $url = 'https://api.crossref.org/works/'.$d.'/agency';
-  $c = curl_init();
-  curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($c, CURLOPT_URL, $url);
-  $curl = curl_exec($c);
-  curl_close($c);
-  $doi = json_decode($curl, true);
-  if($d) {print
-    '<table>
-    <tr><th class="left">DOI</th><th class="right">Registration Agency</th></tr>
-    <tr><td>'.$d.'</td><td>'.$doi["message"]["agency"]["label"].
-    '</td></tr>
-    </table>';
+  print 'd = '.$d.'<br/>';
+  $a = preg_split("/[\r\s]+/", $d);
+  $count = count($a);
+  print 'count = '.$count.'<br/>';
+  print '<table>
+  <tr><th class="left">DOI</th><th class="right">Registration Agency</th></tr>
+  ';
+  for($i=0;$i<$count;$i++) {
+    print 'a['.$i.'] = '.$a[$i].'<br/>';
+    $url = 'https://api.crossref.org/works/'.$a[$i].'/agency';
+    $c = curl_init();
+    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($c, CURLOPT_URL, $url);
+    $curl = curl_exec($c);
+    curl_close($c);
+    $doi = json_decode($curl, true);
+    print
+    '<tr><td>'.$a[$i].'</td><td>'.$doi["message"]["agency"]["label"].
+    '</td></tr>';
   }
+  print '</table>';
   ?>
 
   <hr/>
-  <i>DraID by grolimur - v0.02 - April 4, 2018</i><br/>
+  <i>DraID by grolimur - v0.1 - April 5, 2018</i><br/>
   released under MIT License on <a href="https://github.com/grolimur/DraID" target="_blank">Github</a>
 </body>
 </html>
